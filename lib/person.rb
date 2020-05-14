4e9241b594318c0ab6ad48b1c61ba03345528bf5
Sonja Parsell <hello@sparsell.com>
2020-05-14 19:35:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,8 +4,8 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each do |key, value| 
-      self.send(("#{key}=", value)
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
     end
   end
 
